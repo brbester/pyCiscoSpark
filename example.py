bc1f556ffdfa2db72298f55ed3c07124892d99b6
Brad Bester <brad.bester@gmail.com>
2015-12-23 14:36:16 -0500

    updated return code output

--- example.py
+++ example.py
@@ -5,12 +5,12 @@ accesstoken="Bearer "+str(sys.argv[1])
 print accesstoken
 
 resp = pyCiscoSpark.get_persondetails(accesstoken,"me")
-if resp.status_code != 201:
+if resp.status_code != 200:
     print (format(resp.status_code))
 print(resp.text)
 
 resp = pyCiscoSpark.get_rooms(accesstoken)
-if resp.status_code != 201:
+if resp.status_code != 200:
     print (format(resp.status_code))
 print(resp.text)
 
