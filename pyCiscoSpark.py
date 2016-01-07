c83708cbb43cb5f611eae76839eced1420c7f5d7
Brad Bester <brad.bester@gmail.com>
2016-01-07 11:19:13 -0500

    Bug Fixes

--- pyCiscoSpark.py
+++ pyCiscoSpark.py
@@ -1,6 +1,28 @@
 import requests
 import json
 
+# COMMENTED SECTION BELOW FOR DEBUGGING
+
+#import logging
+
+# These two lines enable debugging at httplib level (requests->urllib3->http.client)
+# You will see the REQUEST, including HEADERS and DATA, and RESPONSE with HEADERS but without DATA.
+# The only thing missing will be the response.body which is not logged.
+#try:
+#    import http.client as http_client
+#except ImportError:
+    # Python 2
+#    import httplib as http_client
+#http_client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+#logging.basicConfig() 
+#logging.getLogger().setLevel(logging.DEBUG)
+#requests_log = logging.getLogger('requests.packages.urllib3')
+#requests_log.setLevel(logging.DEBUG)
+#requests_log.propagate = True
+
+
 #Helpers
 def _url(path):
     return 'https://api.ciscospark.com/v1' + path
@@ -13,13 +35,13 @@ def findroomidbyname (at,roomname):
     return roomid
 
 #GET Requests
-def get_people(at,email="",displayname="",max=10):
+def get_people(at,email='',displayname='',max=10):
     headers = {'Authorization':at}
-    payload = {"max":max}
-    if (email != ""):
-        payload["email"]=email
-    if (displayname != ""):
-        payload["displayName"]=displayname
+    payload = {'max':max}
+    if (email != ''):
+        payload['email']=email
+    if (displayname != ''):
+        payload['displayName']=displayname
     print payload
     resp = requests.get(_url('/people'),params=payload, headers=headers)
     return json.loads(resp.text)
@@ -55,7 +77,7 @@ def get_membership(at,membershipId):
     return json.loads(resp.text)
 
 def get_messages(at,roomId):
-    headers = {'Authorization':at, "content-type":"application/json"}
+    headers = {'Authorization':at, 'content-type':'application/json'}
     payload = {'roomId':roomId}
     resp = requests.get(_url('/messages'),params=payload, headers=headers)
     return json.loads(resp.text)
@@ -77,75 +99,78 @@ def get_webhook(at,webhookId):
 
 #POST Requests
 def post_createroom(at,title):
-    headers = {'Authorization':at, "content-type":"application/x-www-form-urlencoded"}
-    payload = {"title":title}
-    resp = requests.post(url=_url('/rooms'),params=payload, headers=headers)
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'title':title}
+    resp = requests.post(url=_url('/rooms'),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def post_message(at,roomId,text):
-    headers = {'Authorization':at, "content-type":"application/x-www-form-urlencoded"}
-    payload = {'roomId':roomId, "text":text}
-    resp = requests.post(url=_url('/messages'),params=payload, headers=headers)
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'roomId':roomId, 'text':text}
+    resp = requests.post(url=_url('/messages'),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def post_file(at,roomId,url):
-    headers = {'Authorization':at, "content-type":"application/json"}
-    payload = {'roomId':roomId, "files":[url]}
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'roomId':roomId, 'files':[url]}
     resp = requests.post(url=_url('/messages'),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def post_membership(at,roomId,personEmail,isModerator=True):
-    headers = {'Authorization':at, "content-type":"application/x-www-form-urlencoded"}
-    payload = {'roomId':roomId, "personEmail":personEmail, "isModerator":isModerator}
-    resp = requests.post(url=_url('/memberships'),params=payload, headers=headers)
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'roomId':roomId, 'personEmail':personEmail, 'isModerator':isModerator}
+    resp = requests.post(url=_url('/memberships'),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def post_webhook(at,name,targetUrl,resource,event,filter):
-    headers = {'Authorization':at, "content-type":"application/x-www-form-urlencoded"}
-    payload = {'name':name, "targetUrl":targetUrl, "resource":resource, "event":event, "filter":filter}
-    resp = requests.post(url=_url('/webhooks'),params=payload, headers=headers)
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'name':name, 'targetUrl':targetUrl, 'resource':resource, 'event':event, 'filter':filter}
+    resp = requests.post(url=_url('/webhooks'),json=payload, headers=headers)
     return json.loads(resp.text)
 
 #PUTS
-def put_room(at,roomId,title):
-    headers = {'Authorization':at, "content-type":"application/json"}
-    payload = {"title":title}
+def put_room(at,roomId,title='title'):
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'title': title}
     resp = requests.put(url=_url('/rooms/{:s}'.format(roomId)),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def put_membership(at,membershipId,isModerator):
-    headers = {'Authorization':at, "content-type":"application/json"}
-    payload = {"isModerator":isModerator}
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'isModerator':isModerator}
     resp = requests.put(url=_url('/memberships/{:s}'.format(membershipId)),json=payload, headers=headers)
     return json.loads(resp.text)
 
 def put_webhook(at,webhookId,name,targetUrl):
-    headers = {'Authorization':at, "content-type":"application/json"}
-    payload = {"name":name, "targetUrl":targetUrl}
+    headers = {'Authorization':at, 'content-type':'application/json'}
+    payload = {'name':name, 'targetUrl':targetUrl}
     resp = requests.put(url=_url('/webhooks/{:s}'.format(webhookId)),json=payload, headers=headers)
     return json.loads(resp.text)
 
 #DELETES
 
 def del_room(at,roomId):
-    headers = {'Authorization':at, "content-type":"application/json"}
+    headers = {'Authorization':at, 'content-type':'application/json'}
     resp = requests.delete(url=_url('/rooms/{:s}'.format(roomId)), headers=headers)
     return resp.status_code
 
 def del_membership(at,membershipId):
-    headers = {'Authorization':at, "content-type":"application/json"}
+    headers = {'Authorization':at, 'content-type':'application/json'}
     resp = requests.delete(url=_url('/memberships/{:s}'.format(membershipId)), headers=headers)
-    return json.loads(resp.text)
+    return resp.status_code
+
 
 def del_message(at,messageId):
-    headers = {'Authorization':at, "content-type":"application/json"}
+    headers = {'Authorization':at, 'content-type':'application/json'}
     resp = requests.delete(url=_url('/messages/{:s}'.format(messageId)), headers=headers)
-    return json.loads(resp.text)
+    return resp.status_code
+
 
 def del_webhook(at,webhookId):
-    headers = {'Authorization':at, "content-type":"application/json"}
+    headers = {'Authorization':at, 'content-type':'application/json'}
     resp = requests.delete(url=_url('/webhooks/{:s}'.format(webhookId)), headers=headers)
-    return json.loads(resp.text)
+    return resp.status_code
+
 
 
 
